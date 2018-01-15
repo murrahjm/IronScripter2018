3a2c413af339aca80a29c3574a085d3713259e4c
Jeremy Murrah <murrahjm@gmail.com>
2018-01-15 14:15:11 -0600

    add sample code text for reference

--- Prequel1.ps1
+++ Prequel1.ps1
@@ -32,6 +32,31 @@ Function Get-ComputerInfo {
                 Remove-PSSession $Session
                 return
             }
+            If ($Monitor -and $Computer)
         }
     }
 }
+
+
+$Monitor = Get-WmiObject wmiMonitorID -namespace root\wmi
+ $Computer = Get-WmiObject -Class Win32_ComputerSystem 
+ 
+$Monitor ¦ %{     
+    $psObject = New-Object PSObject    
+    $psObject | Add-Member NoteProperty ComputerName ""    
+     $psObject | Add-Member NateProperty ComputerType ""     
+     $psObject | Add-Member NoteProperty ComputerSerial ""       
+     $psObject | Add-Member NoteProperty MonitorSerial ""     
+     $psObject | Add-Member NoteProperty MonitorType "" 
+ 
+    $psObject.ComputerName = $env:computernome     
+    $psObject.ComputerType = $Computer.model     
+    $psObject.ComputerSerial =  $Computer.Name 
+ 
+    $psObject.MonitorSerial = ($_.SerialNumberID -ne 0 | %{[char]$_}) -join ""     
+    $psObject.MonitorType = ($_.UserFriendlyName -ne 0 | %{[char]$_}) -join "" 
+ 
+    $ps0bject 
+ 
+} 
+
