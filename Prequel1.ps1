318af6c321acfb266ac4476dcc0219f355e56bf4
Jeremy Murrah <murrahjm@gmail.com>
2018-01-18 09:48:29 -0600

    updates

--- Prequel1.ps1
+++ Prequel1.ps1
@@ -1,10 +1,10 @@
 Function Get-ComputerInfo {
     Param (
-        [Parameter(Position=1,Mandatory=$True, ValueFromPipelineByPropertyName)]
+        [Parameter(Position=1,Mandatory=$True, ValueFromPipelineByPropertyName=$True, ValueFromPipeline=$True)]
         [alias('Computer','Host','Hostname','Server','ServerName')]
         [String[]]$ComputerName,
 
-        [pscredential]$Credential
+        [pscredential]$Credential = $Null
     )
     BEGIN{
         $ErrorActionPreference = 'Stop'
@@ -15,48 +15,52 @@ Function Get-ComputerInfo {
         Foreach ($Computer in $Computername){
             Write-Verbose "$FunctionName`: Attempting data retrieval via WMI"
             Try {
-                $Monitor = Get-WMIObject -Namespace root\wmi -Class wmiMonitorID -Credential $Credential
-                $Computer = Get-WMIObject -Class Win32_ComputerSystem -Credential $Credential
-                If (! $Monitor){Throw "No monitor data available"}
-                If (! $Computer){Throw "No computer data available"}
+                If ($Credential){
+                    $Monitorinfo = Get-WMIObject -Namespace root\wmi -Class wmiMonitorID -Credential $Credential
+                    $Computerinfo = Get-WMIObject -Class Win32_ComputerSystem -Credential $Credential
+                } else {
+                    $Monitorinfo = Get-WMIObject -Namespace root\wmi -Class wmiMonitorID
+                    $Computerinfo = Get-WMIObject -Class Win32_ComputerSystem
+                    
+                }
+                If (! $Monitorinfo){Throw "No monitor data available"}
+                If (! $Computerinfo){Throw "No computer data available"}
             } Catch {
                 Write-verbose "$FunctionName`: Unable to retrieve data via WMI method.  Attempting WSMan connection."
                 write-verbose "$FunctionName`: Error message $_"
             }
             Try {
-                $Session = New-PSSession -ComputerName $Computer -Credential $Credential
-                $Monitor = Invoke-command -ScriptBlock {Get-WMIObject -namespace root\wmi -class wmiMonitorID} -Session $Session
-                $Computer = Invoke-command -ScriptBlock {Get-WMIObject -class Win32_ComputerSystem} -Session $Session
+                If ($Credential){
+                    $Session = New-PSSession -ComputerName $Computer -Credential $Credential
+                } else {
+                    $Session = New-PSSession -ComputerName $Computer
+                }
+
+                $Monitorinfo = Invoke-command -ScriptBlock {Get-WMIObject -namespace root\wmi -class wmiMonitorID} -Session $Session
+                $Computerinfo = Invoke-command -ScriptBlock {Get-WMIObject -class Win32_ComputerSystem} -Session $Session
             } Catch {
                 write-error "Unable to connect to $Computer"
                 Remove-PSSession $Session
                 return
             }
-            If ($Monitor -and $Computer)
+            $monitors=@()
+            If ($Monitor -and $Computer){
+                foreach ($monitor in $monitorinfo){
+                    $monitors += new-object psobject -properties @{
+                        'MonitorSerial' = ($Monitor.SerialNumberID -ne 0 | %{[char]$_}) -join ""
+                        'MonitorType' = ($monitor.UserFriendlyName -ne 0 | %{[char]$_}) -join ""
+                    }
+                }
+                $output = new-object psobject -properties @{
+                    'ComputerName' = $Computer
+                    'ComputerType' = $Computerinfo.model
+                    'ComputerSerial' = $computerinfo.Name
+                    'MonitorInfo' = $monitors
+                }
+                return $output
+            } else {
+                write-verbose "$FunctionName`: No WMI data found for $computer"
+            }
         }
     }
 }
-
-
-$Monitor = Get-WmiObject wmiMonitorID -namespace root\wmi
- $Computer = Get-WmiObject -Class Win32_ComputerSystem 
- 
-$Monitor ¦ %{     
-    $psObject = New-Object PSObject    
-    $psObject | Add-Member NoteProperty ComputerName ""    
-     $psObject | Add-Member NateProperty ComputerType ""     
-     $psObject | Add-Member NoteProperty ComputerSerial ""       
-     $psObject | Add-Member NoteProperty MonitorSerial ""     
-     $psObject | Add-Member NoteProperty MonitorType "" 
- 
-    $psObject.ComputerName = $env:computernome     
-    $psObject.ComputerType = $Computer.model     
-    $psObject.ComputerSerial =  $Computer.Name 
- 
-    $psObject.MonitorSerial = ($_.SerialNumberID -ne 0 | %{[char]$_}) -join ""     
-    $psObject.MonitorType = ($_.UserFriendlyName -ne 0 | %{[char]$_}) -join "" 
- 
-    $ps0bject 
- 
-} 
-
